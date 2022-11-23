c85aaa15e47929a6fdf36bc8072d55b6e3ef42b9
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2022-11-22 21:08:16 -0500

    Start of trend arrow and last BG difference

--- LoopCaregiver/LoopCaregiver/Views/Home/HUDView.swift
+++ LoopCaregiver/LoopCaregiver/Views/Home/HUDView.swift
@@ -22,18 +22,73 @@ struct HUDView: View {
     }
     
     var body: some View {
-        HStack {
-            Text(formatEGV(nightscoutDateSource.currentEGV))
-                .font(.largeTitle)
-                .foregroundColor(egvValueColor())
-                .padding()
-            Spacer()
-            Picker("Looper", selection: $hudViewModel.selectedLooper) {
-                ForEach(hudViewModel.loopers()) { looper in
-                    Text(looper.name).tag(looper)
+        VStack {
+            HStack {
+                VStack (alignment: .center) {
+                    Text(formatEGV(nightscoutDateSource.currentEGV))
+                        .font(.largeTitle)
+                        .foregroundColor(egvValueColor())
+                        .padding([.leading, .top])
+                    if let lastEGVChange = self.lastEGVChange() {
+                        Text("\(lastEGVChange)")
+                            .font(.footnote)
+                    }
+                }
+                if let egv = nightscoutDateSource.currentEGV {
+                    Image(systemName: arrowImageName(egv: egv))
+                        .foregroundColor(egvValueColor())
+                }
+                Spacer()
+                Picker("Looper", selection: $hudViewModel.selectedLooper) {
+                    ForEach(hudViewModel.loopers()) { looper in
+                        Text(looper.name).tag(looper)
+                    }
                 }
             }
+
         }
+    }
+    
+    func arrowImageName(egv: NightscoutEGV) -> String {
+    
+        guard let trendRate = egv.trendRate else {
+            return "questionmark"
+        }
+        
+        guard let trend = EGVTrend(rawValue: Int(trendRate)) else { //TODO: Could crash on large values
+            return "questionmark"
+        }
+        
+        switch trend {
+            
+        case .doubleUp:
+            return "arrow.up"
+        case .singleUp:
+            return  "arrow.up"
+        case .fortyFiveUp:
+            return  "arrow.up.forward"
+        case .flat:
+            return  "arrow.right"
+        case .fortyFiveDown:
+            return  "arrow.down.forward"
+        case .singleDown:
+            return  "arrow.down"
+        case .doubleDown:
+            return  "arrow.down"
+        case .nonComputable:
+            return  "questionmark"
+        case .outOfRange:
+            return  "questionmark"
+        }
+    }
+    
+    func lastEGVChange() -> Int? {
+        let egvs = nightscoutDateSource.egvs
+        guard egvs.count > 1 else {
+            return nil
+        }
+        
+        return egvs[egvs.count - 1].value - egvs[egvs.count - 2].value
     }
     
     func egvValueColor() -> Color {
@@ -51,7 +106,32 @@ struct HUDView: View {
             return " " //Using spaces, rather than a characterless String, to avoid view elements from jumping during load.
         }
     }
-
+    
+    enum EGVTrend: Int {
+        case doubleUp = 1
+        case singleUp = 2
+        case fortyFiveUp = 3
+        case flat = 4
+        case fortyFiveDown = 5
+        case singleDown = 6
+        case doubleDown = 7
+        case nonComputable = 8
+        case outOfRange = 9
+    }
+    
+    func arrowForTrend(rawValue: Int?) -> String {
+        guard let rawValue, let egvTrend = EGVTrend(rawValue: rawValue) else {
+            return "?"
+        }
+        
+        switch egvTrend {
+        case .doubleUp:
+            return ""
+        default:
+            return ""
+        }
+        
+    }
 }
 
 class HUDViewModel: ObservableObject {
