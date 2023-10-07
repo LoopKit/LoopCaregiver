670064c9efe78b81d745cf8f14109d11139df81a
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2023-10-07 09:39:10 -0400

    Test Target

---
+++ LoopCaregiver/LoopCaregiverTests/OverrideViewModelTests.swift
@@ -0,0 +1,25 @@
+//
+//  OverrideViewModelTests.swift
+//  LoopCaregiverTests
+//
+//  Created by Bill Gestrich on 10/1/23.
+//
+
+import XCTest
+import Combine
+import SwiftUI //TODO: I don't like pulling this in but it is needed for @Binding
+@testable import LoopCaregiver
+
+final class OverrideViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+    
+    func testExample() {
+        
+    }
+
+}
