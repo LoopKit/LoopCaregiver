0f6687a91bc83071d7866c96b6afc73091d9bfa5
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2023-11-23 05:19:57 -0500

    update

--- .github/workflows/workflows.sh
+++ .github/workflows/workflows.sh
@@ -7,15 +7,15 @@ set -o nounset
 
 ## Workflows
 
-#Unused except for local testing.
-function caregiver_identifier() {
+# Unused except for local testing.
+function create_identifier() {
     if ! fastlane caregiver_identifier 2>1 | tee fastlane.log; then
         echo "::error::Could not create identifiers. See error log for details."
         exit 1
     fi
 }
 
-#Unused except for local testing.
+# Unused except for local testing.
 function validate_secrets() {
     if ! fastlane validate_secrets 2>1 | tee fastlane.log; then
         echo "::error::Could not validate secrets. See error log for details."
@@ -28,10 +28,10 @@ function create_certs() {
     if ! fastlane caregiver_cert 2>1 | tee fastlane.log; then
         while read -r line; do
             if [[ "$line" == *"Error cloning certificates repo, please make sure you have read access to the repository you want to use"* ]]; then
-                #It seems there is a race condition with it being created and immediately cloned.
-                errorTitle="Match-Secrets Repository Clone Issue"
+                # There may be a race condition between creating the repo and immediately cloning it.
+                readMeSectionTitle="Match-Secrets Repository Clone Issue"
                 errorMessage="There was an error cloning the Match-Secrets repository. First try running the `Create Certificates` step again. If that fails, check your Github repository access."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             elif [[ "$line" == *"Certificate "* && "$line" == *"(stored in your storage) is not available on the Developer Portal"* ]]; then
                 #Ex: Certificate 'WZUK5NWX3L' (stored in your storage) is not available on the Developer Portal
@@ -39,16 +39,16 @@ function create_certs() {
                 exit 1
             elif [[ "$line" == *"Could not create another Distribution certificate, reached the maximum number of available Distribution certificates."* ]]; then
                 #Occurs if you exceed certs. This can happen if you keep add/delete the Match-Secrets repo during testing.
-                errorTitle="Maximum Certificates Reached"
+                readMeSectionTitle="Maximum Certificates Reached"
                 errorMessage="Cannot create a new certificate. Login to the Apple developer portal and delete unneeded certificates."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             elif [[ "$line" == *"Couldn't find bundle identifier"* && "$line" =~ identifier\ \'([^\']+)\' ]]; then
                 #Ex: Couldn't find bundle identifier 'com.5K844XFC6W.loopkit.LoopCaregiver.LoopCaregiverIntentExtension' for the user ''
                 captured_id="${BASH_REMATCH[1]}"
-                errorTitle="Missing Bundle Identifier"
+                readMeSectionTitle="Missing Bundle Identifier"
                 errorMessage="The app identifier '${captured_id}' is missing from the Apple Developer portal. Resolve this by re-running the 'Add Identifiers' and 'Create Certificates' workflows."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             fi
         done <fastlane.log
@@ -74,33 +74,33 @@ function build() {
             elif [[ "$line" == *"No matching provisioning profiles found for"* ]]; then
                 #I think this is the error when your provisioning profiles are missing.
                 #Note that deleting all your profiles does not trigger this error...?? Maybe when you have an old cert?
-                errorTitle="Provisioning Profiles Invalid"
+                readMeSectionTitle="Provisioning Profiles Invalid"
                 errorMessage="Provisioning profile(s) are invalid. Run the the 'Add Identifiers' and 'Create Certificates' workflows."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             elif [[ "$line" == *"doesn't support the App Groups capability"*  && "$line" =~ \(in\ target\ \'([^\']+)\' ]]; then
                 #Replicate this error by removing the App Group capability from the identifier
                 #Adding the capability and group, then running the Build step resolves it.
                 app_identifier="${BASH_REMATCH[1]}"
-                errorTitle="App Group Capability Missing"
+                readMeSectionTitle="App Group Capability Missing"
                 errorMessage="Resolve this by logging into the Apple Developer portal and add the '$(appGroupName)' app group (where *** is your 10-character TEAMID) to the '${app_identifier}' identifier. Then re-run the 'Create Certificates' and 'Build Caregiver' workflows."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             elif [[ "$line" == *"doesn't match the entitlements file's value for the com.apple.security.application-groups entitlement"* && "$line" =~ \(in\ target\ \'([^\']+)\' ]]; then
                 app_identifier="${BASH_REMATCH[1]}"
                 #Missing or wrong App Group but the group capablity is added.
                 #Ex: error: Provisioning profile "match AppStore com.5K844XFC6W.loopkit.LoopCaregiver.LoopCaregiverWidgetExtension" doesn't match the entitlements file's value for the com.apple.security.application-groups entitlement. (in target 'LoopCaregiverWidgetExtension' from project 'LoopCaregiver')[0m
-                errorTitle="Bundle Identifier Missing App Group"
+                readMeSectionTitle="Bundle Identifier Missing App Group"
                 errorMessage="A bundle identifier is missing the required app group. Resolve this by logging into the Apple Developer portal and add the '$(appGroupName)' app group (where *** is your 10-character TEAMID) to the '${app_identifier}' identifier. Then re-run the 'Create Certificates' and 'Build Caregiver' workflows."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             elif [[ "$line" == *"doesn't include signing certificate "* && "$line" =~ \(in\ target\ \'([^\']+)\' ]]; then
                 #This can happen if you delete the Match repo and skip the `Create Certificates` step.
                 #Ex: error: Provisioning profile "match AppStore com.5K844XFC6W.loopkit.LoopCaregiver.LoopCaregiverWatchApp" doesn't include signing certificate "Apple Distribution: William Gestrich (5K844XFC6W)". (in target 'LoopCaregiverWatchApp' from project 'LoopCaregiver')
                 app_identifier="${BASH_REMATCH[1]}"
-                errorTitle="Missing Signing Certificates"
+                readMeSectionTitle="Missing Signing Certificates"
                 errorMessage="The provisioning profile for $app_identifier is missing its signing certificate. To resolve, run the 'Create Certificates' workflow again."
-                logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+                logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
                 exit 1
             fi
         done <fastlane.log
@@ -111,7 +111,7 @@ function build() {
     fi
 }
 
-#Unused except for local testing.
+# Unused except for local testing.
 function release() {
     if ! fastlane caregiver_release 2>1 | tee fastlane.log; then
         #Default
@@ -131,27 +131,27 @@ function appGroupName() {
 function logMissingCertificateError() {
     #Occurs after deleting your certificate. No steps seem to fix this, except deleting the Match-Secrets repo.
     line="$1"
-    errorTitle="Certificate is Missing"
+    readMeSectionTitle="Certificate is Missing"
     errorMessage="Certificate is missing from the Apple developer portal. Resolve this by deleting the Github Match-Secrets repository. Then run all Gihub workflows again."
-    logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+    logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
 }
 
 function logMissingMatchRepoError() {
     #Occurs after deleting the match repo. The Validate Secrets step will recreate it.
     line="$1"
-    errorTitle="Match Repository Missing"
+    readMeSectionTitle="Match Repository Missing"
     errorMessage="The Match-Secrets repository is missing. To resolve, run the 'Validate Secrets' step again."
-    logErrorMessages "${errorTitle}" "${errorMessage}" "${line}"
+    logErrorMessages "${readMeSectionTitle}" "${errorMessage}" "${line}"
 }
 
 function logErrorMessages() {
-    errorTitle="$1"
+    readMeSectionTitle="$1"
     errorMessage="$2"
     rawError="$3"
-    readMETag="#$(echo $errorTitle | tr " " "-")"
+    readMeID="$(echo $readMeSectionTitle | tr " " "-")"
     branchName=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}
     readMEURL="${REPO_URL}/blob/${branchName}/fastlane/testflight.md"
-    echo "::error title=$errorTitle::$errorMessage For more details on this error: ${readMEURL}/${readMETag}"
+    echo "::error title=$readMeSectionTitle::$errorMessage For more details on this error: ${readMEURL}/#${readMeID}"
     echo "::error title=Fastlane Details::$rawError"
 }
 
