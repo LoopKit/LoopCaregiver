0e96fe03201271b9b52e4468aaf08b53f21a548e
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2024-01-20 07:34:48 -0500

    Add missing

--- LoopCaregiver/LoopCaregiverWatchApp/HomeView.swift
+++ LoopCaregiver/LoopCaregiverWatchApp/HomeView.swift
@@ -91,6 +91,6 @@ struct HomeView: View {
     let composer = ServiceComposerPreviews()
     return NavigationStack {
         let looper = composer.accountServiceManager.selectedLooper!
-        HomeView(looperService: composer.accountServiceManager.createLooperService(looper: looper, settings: composer.settings))
+        HomeView(connectivityManager: WatchConnectivityManager.shared, looperService: composer.accountServiceManager.createLooperService(looper: looper, settings: composer.settings))
     }
 }
